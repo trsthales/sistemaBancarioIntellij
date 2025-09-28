915feb330784d6c14d37a3fae8b8830cf120a438
thales87 <thales87@gmail.com>
2025-09-28 01:30:04 -0300

    adicionando classe TodosOsTestes

--- src/test/java/thales/GerenciaContasTest.java
+++ src/test/java/thales/GerenciaContasTest.java
@@ -2,6 +2,7 @@ package thales;
 
 import negocio.ContaCorrente;
 import negocio.GerenciadoraContas;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
@@ -13,15 +14,19 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class GerenciaContasTest {
-    ContaCorrente contaCorrente1 = new ContaCorrente(1,250,true);
-    ContaCorrente contaCorrente2 = new ContaCorrente(2,350,true);
 
-    List<ContaCorrente> listaContas = new ArrayList<ContaCorrente>();
-
-    GerenciadoraContas gerenciadoraContas;
+    private GerenciadoraContas gerenciadoraContas;
+    private ContaCorrente contaCorrente1;
+    private ContaCorrente contaCorrente2;
+    private List<ContaCorrente> listaContas;
 
     @Before
     public void setUp(){
+        contaCorrente1 = new ContaCorrente(1,250,true);
+        contaCorrente2 = new ContaCorrente(2,350,true);
+
+        listaContas = new ArrayList<ContaCorrente>();
+
         listaContas.add(contaCorrente1);
         listaContas.add(contaCorrente2);
 
@@ -44,5 +49,10 @@ public class GerenciaContasTest {
         assertTrue(deuCerto);
         assertThat(contaCorrente1.getSaldo(),is(-50.0));
         assertThat(contaCorrente2.getSaldo(),is(650.0));
+    }
+
+    @After
+    public void tearDown(){
+        listaContas.clear();
     }
 }
---
+++ src/test/java/thales/TodosOsTestes.java
@@ -0,0 +1,12 @@
+package thales;
+
+import negocio.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ GerenciaClientesTest.class, GerenciaContasTest.class })
+public class TodosOsTestes {
+
+}
